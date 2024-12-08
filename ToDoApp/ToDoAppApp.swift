9bd26a1096010c1532557322197eed857906d578
ir <Isabelaruiz147@gmail.com>
2024-12-08 13:57:30 -0500

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Isabela Ruiz Bustos on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
